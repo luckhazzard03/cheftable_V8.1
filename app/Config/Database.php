57dc46b3d93dcb0bacea81fd5eefd7a4fdcf03cf
luckhazzard03 <lordart03@gmail.com>
2024-12-10 00:33:55 -0500

    se agrega contraseña para database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => '',
+        'password'     => '1234567890',
         'database'     => 'chef_table7',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
